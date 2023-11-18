1124d81a9843917505b8b9b068dc1c44c0c18caf
Apisit <apisit.mudsang@gmail.com>
2023-11-18 19:15:54 +0700

    Add login session

--- app.js
+++ app.js
@@ -14,19 +14,60 @@ mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
 
 const userSchema = new mongoose.Schema({
   email: String,
-  password: String
+  password: String,
 });
 
-const User = new mongoose.Model({'user': userSchema});
+const User = new mongoose.model("User", userSchema);
 
-app.get("/", function (req, res) {
-  res.send("home");
+app.get("/", (req, res) => {
+  res.render("home");
 });
 
-app.get("/login", function (req, res) {
-  res.send("login");
+app.get("/login", (req, res) => {
+  res.render("login");
 });
 
-app.listen(3000, function(req, res) {
-    console.log("listening on port 3000");
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.post("/register", (req, res) => {
+  const newUser = new User({
+    email: req.body.email,
+    password: req.body.password,
+  });
+
+  newUser
+    .save()
+    .then(() => {
+      res.render("secrets");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.post("/login", (req, res) => {
+  const username = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({ email: username })
+    .then((foundUser) => {
+      if (foundUser) {
+        if (foundUser.password === password) {
+          res.render("secrets");
+        } else {
+          res.render("login");
+        }
+      } else {
+        res.render("login");
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.listen(3000, (req, res) => {
+  console.log("listening on port 3000");
 });
